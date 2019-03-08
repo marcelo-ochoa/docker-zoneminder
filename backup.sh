92de366bd75a1f093fdda5559be402b5641efc47
Marcelo Ochoa <mochoa@temperies.com>
2019-03-08 10:51:18 -0300

    fix destination dir for backups

--- backup.sh
+++ backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Backup mysql
-mysqldump -u root -pmysqlpsswd -h ${ZM_DB_HOST:-localhost} --all-databases | gzip > /backup/alldb_backup-`date +'%Y%m%d%H%M%S'`.sql.gz
+mysqldump -u root -pmysqlpsswd -h ${ZM_DB_HOST:-localhost} --all-databases | gzip > /var/backups/alldb_backup-`date +'%Y%m%d%H%M%S'`.sql.gz
 
 #Backup important file ... of the configuration ...
 cp  /etc/hosts  /var/backups/
